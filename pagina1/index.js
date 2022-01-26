aaa4acb787567ad713203886ff4c862c9e0aa671
Juliano Rodrigo <83507327+Julian01001111@users.noreply.github.com>
2022-01-26 12:46:49 -0300

    js

--- pagina1/index.js
+++ pagina1/index.js
@@ -1,10 +1,11 @@
+
+var venda = document.getElementById("ven");
+
 function estoqueZero() {
     alert('O carro ja foi vendido.');
 }
-var venda = document.getElementById("ven");
-
 venda.addEventListener('click', estoqueZero);
 
-
+prompt('Ola')
 
 
