17f3f8aff9ff4a70c7ff53cbf7526136e999930a
Juliano Rodrigo <83507327+Julian01001111@users.noreply.github.com>
2022-01-26 12:40:31 -0300

    Funcao js

--- pagina1/index.js
+++ pagina1/index.js
@@ -1,3 +1,10 @@
-function pesquisa(){
-if 
-};
+function estoqueZero() {
+    alert('O carro ja foi vendido.');
+}
+var venda = document.getElementById("ven");
+
+venda.addEventListener('click', estoqueZero);
+
+
+
+
