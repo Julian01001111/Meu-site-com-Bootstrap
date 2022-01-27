84ae0e146716dbde11e293aa9321485f67f1007d
Juliano Rodrigo <83507327+Julian01001111@users.noreply.github.com>
2022-01-27 11:58:58 -0300

    Update index.js

--- pagina1/index.js
+++ pagina1/index.js
@@ -1,13 +1,15 @@
 var total = 3;
 
 function incrementClick() {
-   if (total>0) updateDisplay(--total);
-    if(total>0)
+
+    if(total>1)
     alert("Otima compra")
-    else
-    alert('O ultimo acabou de ser vendido.');
+
+    if (total==1)  alert('Você comprou o ultimo, otima compra.');
+
     if (total==0) 
     alert("Estoque indisponivel")
+    if (total>0) updateDisplay(--total);
 }
 function updateDisplay(val) {
     document.getElementById("vendeu").innerHTML = val;
